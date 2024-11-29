bf6236bcd7ca2cb74cb10598ccea1068d349fd6a
Raseel <raseelp321@gmail.com>
2024-11-29 22:34:25 +0530

    made help for vibe select

--- lib/util/help.dart
+++ lib/util/help.dart
@@ -92,4 +92,109 @@ class Help {
       },
     );
   }
+
+  void vibeSelectHelp(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Stack(children: [
+          AlertDialog(
+            backgroundColor: Colors.black.withOpacity(0.5),
+            content: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.width * 1.7,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .25,
+                  ),
+                  const Text(
+                    'You again?',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .03,
+                  ),
+                  const Text(
+                    'So in here you can select what kind of card you wanna select, ',
+                    style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .05,
+                  ),
+                  const Text(
+                    'you can select from the three options,',
+                    style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .07,
+                  ),
+                  const Text(
+                    'Ohio Unleashed: Pure chaos with iconic Ohio memes.',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .03,
+                  ),
+                  const Text(
+                    'Skibidi Blast: Skibidi Toilet madness in card form.',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .03,
+                  ),
+                  const Text(
+                    'Rizz Rush: Rizzlords and Gigachads dominate.',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.width * .03,
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Got it',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'bangers',
+                            color: Colors.white),
+                      ))
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              left: MediaQuery.of(context).size.width * 0.2,
+              child: Image.asset(
+                'assets/images/untilbg.png',
+                width: MediaQuery.of(context).size.width * 0.6,
+              ))
+        ]);
+      },
+    );
+  }
 }
